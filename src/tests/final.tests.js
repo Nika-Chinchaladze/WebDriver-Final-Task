31df811beba27b4e0b877c2c24879bc11cf3e030
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2024-07-15 21:06:09 +0400

    UC-2 task is completed

--- src/tests/final.tests.js
+++ src/tests/final.tests.js
@@ -13,8 +13,8 @@ describe("Swag Labs Page", () => {
         await page("login").passwordInput.setValue("chincho123");
     
         // Clear the inputs.
-        await page("login").usernameInput.clearValue();
-        await page("login").passwordInput.clearValue();
+        await page("login").usernameInput.setValue("");
+        await page("login").passwordInput.setValue("");
 
         const username = await page("login").usernameInput.getValue();
         const password = await page("login").passwordInput.getValue();
@@ -32,5 +32,29 @@ describe("Swag Labs Page", () => {
     
         expect(errorElement).toExist();
         expect(errorMessage).toContain("Username is required");
+    });
+    
+    it("should test UC-2 task", async () => {
+        // Type any credentials in username & Enter password.
+        await page("login").usernameInput.setValue("chincho");
+        await page("login").passwordInput.setValue("chincho123");
+
+        // Clear the "Password" input.
+        await page("login").passwordInput.setValue("");
+
+        // Hit the "Login" button.
+        const value = await page("login").passwordInput.getValue();
+        expect(value).toBe("");
+        
+        await browser.refresh();
+        await page("login").usernameInput.setValue("chincho");
+        await page("login").submitBtn.click();
+
+        // Check the error messages: "Password is required".
+        const errorElement = await page("error").userNameRequired;
+        const errorMessage = await errorElement.getText();
+    
+        expect(errorElement).toExist();
+        expect(errorMessage).toContain("Password is required");
     });
 });
